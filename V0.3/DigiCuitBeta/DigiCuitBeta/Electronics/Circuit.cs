077d9b74cc82de1284bfa24d1dcaffb9653e7dcb
Hans-Eliseo Fernández <hanseliseofk@gmail.com>
2015-01-16 02:31:02 +0100

    Poniendo bases y actualizando licencia

---
+++ V0.3/DigiCuitBeta/DigiCuitBeta/Electronics/Circuit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiCuitBeta.Electronics
+{
+    public class Circuit
+    {
+        public class GirdNode
+        {
+            public class Pointer
+            {
+                public int ComponentIndex { set; get; }
+                public int InOutIndex { set; get; }
+                public override string ToString()
+                { return String.Format("{0}:{1}", this.ComponentIndex.ToString(), this.InOutIndex.ToString()); }
+            }
+            public Pointer Plug { get; set; }
+            public Pointer Socket { get; set; }
+        }
+        
+        public Size GirdSize { get; set; }
+        public GirdNode[,] Nodes { get; set; }
+        public ComponentCollection Components { set; get; }
+    }
+}
---
+++ V0.3/DigiCuitBeta/DigiCuitBeta/Electronics/ComponentCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigiCuitBeta.Electronics
+{
+    public class ComponentCollection:IList<Component>
+    {
+        public int IndexOf(Component item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, Component item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Component this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(Component item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Component item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Component[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(Component item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Component> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
